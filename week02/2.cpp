bda3f8566a8d1da0651579826f9bd233aba15e21
Beisenbek Baisakov <bbaisakov@dar.kz>
2022-09-10 10:50:45 +0600

    upd

---
+++ week02/2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+bool f(int x){
+    bool result = true;
+
+    if(x == 1){
+        result = false;
+    }
+
+    for(int i = 2; i <= sqrt(x); ++i){
+        cout << i << " ";
+        if(x % i == 0){
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+}
+
+
+int main(){
+
+    int x;
+    cin >> x;
+    cout << f2(x);
+
+    return 0;
+}
