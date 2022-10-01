0b720a241a107da7949d6608c5a48fde32ac72c2
Beisenbek Baisakov <beysenbek@gmail.com>
2022-10-01 11:42:52 +0600

    'upd'

---
+++ lecture/week05/1.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+struct node{
+    int val;
+    node * left;
+    node * right;
+    int cnt;
+    node(int val){
+        this->val = val;
+        this->left = this->right = NULL;
+    }
+};
+
+struct bst{
+    public:
+    bst(){
+        root = NULL;
+    }
+    void add(int x){
+        node * result = this->add(this->root, x);
+        if(root == NULL){
+            root = result;
+        }
+    }
+    void print(){
+        this->print(this->root);
+    }
+
+    private:
+    node * root;
+    node * add(node * current, int x){
+        if(current == NULL){
+            current = new node(x);
+        }else if(current->val > x){
+            if(current->left == NULL){
+                current->left = add(current->left, x);
+            }else{
+                add(current->left, x);
+            }
+        }else if(current->val < x){
+            if(current->right == NULL){
+                current->right = add(current->right, x);
+            }else{
+                add(current->right, x);
+            }
+        }
+        return current;
+    }
+    void print(node * current){
+        if(current != NULL){
+            print(current->left);
+            cout << current->val << " ";
+            print(current->right);
+        }
+    }
+};
+
+
+int main(){
+
+    int a[] = {10, 11, 8, 5, 4, 12};
+    int n = sizeof(a) / sizeof(int);
+
+    bst * b = new bst();
+
+    for(int i = 0; i < n; ++i){
+        b->add(a[i]);
+    }
+
+    b->print();
+
+    return 0;
+}
