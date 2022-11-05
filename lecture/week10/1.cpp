93b76b8527f264aab31397bf3e340327637ebe00
Beisenbek Baisakov <beysenbek@gmail.com>
2022-11-05 15:18:56 +0600

    'upd'

--- lecture/week10/1.cpp
+++ lecture/week10/1.cpp
@@ -6,32 +6,51 @@ int f1(char c){
     return c - int('a') + 1;
 }
 
-void f(string t){
+void f(string t, string s){
     int n = t.size();
-    int h[n];
-    int p = 1;
-    int q = (1 << 20) + 1;
+    int m = s.size();
+
+    long long h[n];
+    long long p[n];
+    long long q = (1 << 30) + 1;
+
+    p[0] = 1;
+
+    for(int i = 1; i < max(n, m); ++i){
+        p[i] = (p[i - 1] * 31) % q;
+    }
 
     for(int i = 0; i < n; ++i){
         if(i == 0){
-            h[i] = (f1(t[i]) * p) % q;
+            h[i] = (f1(t[i]) * p[i]) % q;
         }else{
-            h[i] = (h[i - 1] + (f1(t[i]) * p) % q ) % q;
+            h[i] = (h[i - 1] + (f1(t[i]) * p[i]) % q ) % q;
         }
-        p = (p * 31) % q;
+    }
+    long long h_s = 0;
+    for(int i = 0; i < m; ++i){
+        h_s += (f1(s[i]) * p[i]) % q;
     }
 
-    for(int i = 0; i < n; ++i){
-        cout << h[i] << endl;
+    for(int i = 0; i <= n - m; i++){
+        long long d = h[i+m-1];
+        if(i > 0){
+            d -= h[i-1];
+        }
+        if( d == (h_s * p[i]) % q){
+            cout << "found " << i << endl;
+        }
     }
+
 }
 
 int main(){
 
-    string text;
+    string text, pattern;
     getline(cin, text);
+    getline(cin, pattern);
 
-    f(text);
+    f(text, pattern);
 
 
     return 0;
